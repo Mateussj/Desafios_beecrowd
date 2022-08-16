98daa89536d82ca0a3b62c7f125a5d1636a98527
Mateus Juvencio <mateusjuvencio03@gmail.com>
2022-08-15 21:52:23 -0300

    Update desafio 1162

--- JavaScript/1162_Train_Swapping.js
+++ JavaScript/1162_Train_Swapping.js
@@ -1,28 +1,28 @@
 const arquivo = require('fs').readFileSync('./input.txt', 'utf8');
 const linhas = arquivo.split('\n');
 
-function main () {
+function main() {
     const nr_testes = parseInt(linhas[0]);
-        for (let i = 1; i <= nr_testes; i++) {
-            const qtd_trocas = contar_trocas(linhas[i * 2].split(' '));
-            console.log(`Optimal train swapping takes ${qtd_trocas} swaps.`);          
-        }
+    for (let i = 1; i <= nr_testes; i++) {
+        const qtd_trocas = contar_trocas(linhas[i * 2].split(' '));
+        console.log(`Optimal train swapping takes ${qtd_trocas} swaps.`);
+    }
 }
 
-function contar_trocas (vagoes) {
+function contar_trocas(vagoes) {
     let quantidade_de_trocas = 0;
     const quantidade_de_vagoes = vagoes.length;
-    for(let i = 0; i < quantidade_de_vagoes; i++){
-        for(let j = 0; j < quantidade_de_vagoes -1; j++){
+    for (let i = 0; i < quantidade_de_vagoes; i++) {
+        for (let j = 0; j < quantidade_de_vagoes - 1; j++) {
             let vagao_atual = parseInt(vagoes[j]);
-            let prox_vagao = parseInt(vagoes[j+1]);
+            let prox_vagao = parseInt(vagoes[j + 1]);
             if (vagao_atual > prox_vagao) {
                 let auxilar = vagao_atual;
                 vagoes[j] = prox_vagao;
                 vagoes[j + 1] = auxilar;
                 quantidade_de_trocas++
             }
-        }    
+        }
     }
     return quantidade_de_trocas;
 }
